1158a085fad4d63a98f2edcce0b73fbfbe58add9
Kevin2517 <kevinlee1827@gmail.com>
2022-12-06 12:00:15 +0800

    week14

---
+++ week14/week14-1.cpp
@@ -0,0 +1,17 @@
+///狡策:俱计跑计 int a; a琌俱计
+///狡策:ㄧ计(量ウ)
+///int sum( int a,int b ); ウ
+
+#include <stdio.h>
+
+int sum( int a, int b );
+
+int main()
+{///ノぇ玡璶declare┪﹚竡define
+    int ans = sum(2, 3); ///ㄏノ ㊣
+    printf("ans: %d\n", ans);
+}
+int sum( int a, int b )///﹚竡 define
+{
+    return a + b;
+}///肚 滦產
